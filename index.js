7a773f86099d84a4c899a81ab40f2131e4f30a92
GERMAIN ARTURO DURAN TORRES <arturodurantorres@gmail.com>
2018-09-10 17:07:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Star
 var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
 function theBeatlesPlay (ArrayMusicians,ArrayInstruments){
+<<<<<<< HEAD
    var Array = [];
    var MLenght= ArrayMusicians.length;
   
@@ -12,13 +13,27 @@ function theBeatlesPlay (ArrayMusicians,ArrayInstruments){
    }
    
   return Array;
+=======
+   let Array=[];
+   var MLenght= ArrayMusicians.lenght;
+  
+   for (var i=0; i < MLenght; i++){
+     Array.push(ArrayMusicians[i] +'plays'+ ArrayInstruments[i]);
+   }
+   
+   return Array;
+>>>>>>> 7f2e954b7ed91e57975a244d948d4edcd4fb960c
 }
 const facts = ["He was the last Beatle to learn to drive","He was never a vegetarian","He was a choir boy and boy scout","He hated the sound of his own voice"];
 
 function johnLennonFacts (ArrayFacts){
   var i=0;
   var Array=[];
+<<<<<<< HEAD
   while (i < ArrayFacts.length){
+=======
+  while (i < ArrayFacts.lenght){
+>>>>>>> 7f2e954b7ed91e57975a244d948d4edcd4fb960c
     Array.push(ArrayFacts[i] + "!!!"); 
     i++;  
   }
@@ -30,7 +45,11 @@ function iLoveTheBeatles (n){
   var Array=[];
   
   do{
+<<<<<<< HEAD
     Array.push('I love the Beatles!');
+=======
+    Array.push("I love the Beatles!");
+>>>>>>> 7f2e954b7ed91e57975a244d948d4edcd4fb960c
     n++;
   }
   while (n<15);
